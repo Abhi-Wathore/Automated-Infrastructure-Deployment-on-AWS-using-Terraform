6921137af3bbbf153e4a1d0b5e2b3402644e6089
Abhishek Wathore <abhishekbwathore@gmail.com>
2025-05-31 23:57:20 +0530

    VPC Module

--- VPC/output.tf
+++ VPC/output.tf
@@ -1,7 +1,7 @@
 
 #This file contains the output values for the VPC module.
 # This file is used to expose key attributes of the VPC and its related resources to other modules or to the root module. 
-
+# It includes outputs for VPC ID, subnet IDs, CIDR blocks, route table ID, internet gateway ID, and security group ID.
 output "vpc_id" {
 description = "The ID of the VPC"
 value = aws_vpc.mainvpc.id
