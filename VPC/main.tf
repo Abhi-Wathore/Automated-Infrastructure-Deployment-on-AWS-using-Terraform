783aafeb3126e7fb2ba36faa03c419da6ded303e
Abhishek Wathore <abhishekbwathore@gmail.com>
2025-06-01 19:43:14 +0530

    VPC Module

--- VPC/main.tf
+++ VPC/main.tf
@@ -3,58 +3,79 @@ resource "aws_vpc" "mainvpc" {
   tags = {
     Name = var.vpc_name
   }
-    # The VPC is created with the CIDR block defined in the variable vpc_cidr
-  
-}
-resource "aws_subnet" "pub-subnet" {
-  vpc_id            = aws_vpc.mainvpc.id
-  cidr_block        = var.pub_subnet_cidr
-  map_public_ip_on_launch = true
-    availability_zone = var.availability_zone
-    tags = {
-      Name = var.pub_subnet_name
-    }
-    # The map_public_ip_on_launch attribute is set to true to assign public IPs to instances in this subnet
 }
 
-resource "aws_subnet" "pvt-subnet" {
+resource "aws_subnet" "pub_subnet" {
+  vpc_id                  = aws_vpc.mainvpc.id
+  cidr_block              = var.pub_subnet_cidr
+  map_public_ip_on_launch = true
+  availability_zone       = var.availability_zone
+  tags = {
+    Name = var.pub_subnet_name
+  }
+}
+
+resource "aws_subnet" "pvt_subnet" {
   vpc_id            = aws_vpc.mainvpc.id
   cidr_block        = var.pvt_subnet_cidr
   availability_zone = var.availability_zone
   tags = {
     Name = var.pvt_subnet_name
   }
-    # The private subnet does not have the map_public_ip_on_launch attribute set, so instances in this subnet will not receive public IPs
-  
 }
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.mainvpc.id
-    # The internet gateway allows communication between instances in the VPC and the internet
-}
-
-resource "aws_route_table_association" "pub-subnet-assoc" {
-  subnet_id      = aws_subnet.pub-subnet.id
-  route_table_id = aws_route_table.myroute.id
-    # This associates the public subnet with the route table
-}
-resource "aws_route_table_association" "pvt-subnet-assoc" {
-  subnet_id      = aws_subnet.pvt-subnet.id
-  route_table_id = aws_route_table.myroute.id
-    # This associates the private subnet with the route table
-    # The private subnet does not have a route to the internet, so instances in this subnet will not be able to access the internet directly
-    # You may want to add a NAT gateway for instances in the private subnet to access the internet
 }
 
 resource "aws_route_table" "myroute" {
   vpc_id = aws_vpc.mainvpc.id
+
   route {
     cidr_block = var.route_cidr
     gateway_id = aws_internet_gateway.igw.id
-    # The route_cidr variable should be defined in your variables.tf file
-}
+  }
 }
 
-data "aws_security_group" "mysg" {
-  vpc_id = aws_vpc.mainvpc.id  
+resource "aws_route_table_association" "pub_subnet_assoc" {
+  subnet_id      = aws_subnet.pub_subnet.id
+  route_table_id = aws_route_table.myroute.id
 }
+
+resource "aws_route_table_association" "pvt_subnet_assoc" {
+  subnet_id      = aws_subnet.pvt_subnet.id
+  route_table_id = aws_route_table.myroute.id
+}
+
+# Option 1: Referencing existing security group by name
+data "aws_security_group" "mysg" {
+  name   = var.security_group_name
+  vpc_id = aws_vpc.mainvpc.id
+}
+
+# OR
+
+# Option 2: Create a new security group instead
+# resource "aws_security_group" "mysg" {
+#   name        = var.security_group_name
+#   description = "Allow web traffic"
+#   vpc_id      = aws_vpc.mainvpc.id
+
+#   ingress {
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+#   tags = {
+#     Name = var.security_group_name
+#   }
+# }
