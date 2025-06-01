9ef2e7df5d03cabcac6cf2b0d3140c8ee1431337
Abhishek Wathore <abhishekbwathore@gmail.com>
2025-06-01 19:44:37 +0530

    Variable file

--- variable.tf
+++ variable.tf
@@ -1,175 +1,174 @@
-# This file defines the variables used in the Terraform configuration for AWS resources.
-# It includes variables for VPC, subnets, security groups, EC2 instances, and S3 buckets.
+# VPC Configuration
+
 variable "vpc_name" {
-    description = "Name of the VPC"
-    type        = string
-    default     = "my-vpc"
+  description = "Name of the VPC"
+  type        = string
+  default     = "my-vpc"
 }
 
 variable "vpc_cidr" {
-    description = "CIDR block for the VPC"
-    type        = string
-    default     = "10.0.0.0/16"
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "pub_subnet_cidr" {
-    description = "CIDR block for the public subnet"
-    type        = string
-    default     = "10.0.1.0/24"
+  description = "CIDR block for the public subnet"
+  type        = string
+  default     = "10.0.1.0/24"
 }
 
 variable "pvt_subnet_cidr" {
-    description = "CIDR block for the private subnet"
-    type        = string
-    default     = "10.0.2.0/24"
+  description = "CIDR block for the private subnet"
+  type        = string
+  default     = "10.0.2.0/24"
 }
 
 variable "availability_zone" {
-    description = "Availability zone for the subnets"
-    type        = string
-    default     = "eu-north-1a"
+  description = "Availability zone to deploy subnets"
+  type        = string
+  default     = "eu-north-1a"
 }
 
 variable "route_cidr" {
-    description = "CIDR block for the route table"
-    type        = string
-    default     = "0.0.0.0/0"
+  description = "CIDR for the default route (usually 0.0.0.0/0)"
+  type        = string
+  default     = "0.0.0.0/0"
 }
 
 variable "security_group_name" {
-    description = "Name of the security group"
-    type        = string
-    default     = "my-security-group"
+  description = "Name of the default security group"
+  type        = string
+  default     = "my-security-group"
 }
 
 variable "pub_subnet_name" {
-    description = "Name of the public subnet"
-    type        = string
-    default     = "public-subnet"
+  description = "Name tag for public subnet"
+  type        = string
+  default     = "public-subnet"
 }
 
 variable "pvt_subnet_name" {
-    description = "Name of the private subnet"
-    type        = string
-    default     = "private-subnet"
+  description = "Name tag for private subnet"
+  type        = string
+  default     = "private-subnet"
 }
 
+# EC2/Instances Configuration
+
 variable "aws_region" {
-    description = "AWS region to deploy resources"
-    type        = string
-    default     = "eu-north-1"
+  description = "AWS region to deploy the infrastructure"
+  type        = string
+  default     = "eu-north-1"
 }
 
 variable "instance_name" {
-    description = "Name of the EC2 instance"
-    type        = string
-    default     = "my-instance"
+  description = "Name tag for the EC2 instances"
+  type        = string
+  default     = "my-instance"
 }
 
 variable "instance_type" {
-    description = "Type of the EC2 instance"
-    type        = string
-    default     = "t3.micro"
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
 }
 
 variable "project_name" {
-    description = "Name of the project"
-    type        = string
-    default     = "my-project"
+  description = "Project name tag for resources"
+  type        = string
+  default     = "my-project"
 }
 
 variable "environment" {
-    description = "Environment for the resources (e.g., dev, prod)"
-    type        = string
-    default     = "dev"
+  description = "Environment tag (e.g. dev, staging, prod)"
+  type        = string
+  default     = "dev"
 }
 
 variable "key_name" {
-    description = "Name of the key pair for SSH access"
-    type        = string
-    default     = "new-key"
+  description = "SSH key pair name for EC2 access"
+  type        = string
+  default     = "new-key"
 }
 
 variable "ami" {
-    description = "AMI ID for the EC2 instance"
-    type        = string
-    default     = "ami-0c1ac8a41498c1a9c" # Example AMI ID, replace with a valid one
+  description = "AMI ID for EC2 instances"
+  type        = string
+  default     = "ami-0c1ac8a41498c1a9c" # Example AMI for eu-north-1
 }
 
+# S3 Configuration (used by EC2 module or others)
+
 variable "bucket_name" {
-    description = "Name of the S3 bucket"
-    type        = string
-    default     = "my1-2unique-3bucket-4name" # Ensure this is globally unique
+  description = "S3 bucket name (must be globally unique)"
+  type        = string
+  default     = "my-unique-bucket-name-123"
 }
 
 variable "bucket_region" {
-    description = "Region for the S3 bucket"
-    type        = string
-    default     = "eu-north-1"
+  description = "Region for the S3 bucket"
+  type        = string
+  default     = "eu-north-1"
 }
 
 variable "bucket_key" {
-    description = "Key for the S3 bucket object"
-    type        = string
-    default     = "my-object-key"
+  description = "Object key for storing in the bucket"
+  type        = string
+  default     = "my-app-key"
 }
- 
+
+# Load Balancer Configuration
+
 variable "aws_lb_name" {
+  description = "Name of the Load Balancer"
   type        = string
-  description = "Name of the load balancer"
-}
-
-variable "vpc_id" {
-  type        = string
-  description = "VPC ID for the load balancer"
-}
-
-variable "aws_lb_listener_name" {
-  type        = string
-  description = "Name of the load balancer listener"
-}
-
-variable "aws_lb_target_group_name" {
-  type        = string
-  description = "Name of the target group"
+  default     = "my-lb"
 }
 
 variable "aws_lb_internal" {
+  description = "Whether the Load Balancer is internal (true) or internet-facing (false)"
   type        = bool
-  description = "Whether the LB is internal"
+  default     = false
 }
 
 variable "aws_lb_security_groups" {
+  description = "List of security group IDs to associate with the Load Balancer"
   type        = list(string)
-  description = "List of security groups for the LB"
 }
 
 variable "aws_lb_subnets" {
+  description = "List of subnet IDs for the Load Balancer"
   type        = list(string)
-  description = "List of subnets for the LB"
 }
 
+variable "aws_lb_target_group_name" {
+  description = "Name of the target group attached to the Load Balancer"
+  type        = string
+  default     = "my-target-group"
+}
+variable "aws_lb_listener_name" {
+  description = "Name of the Load Balancer listener"
+  type        = string
+  default     = "my-listener"
+}
 
+# Auto Scaling Group Configuration
+
+variable "desired_capacity" {
+  description = "Desired number of EC2 instances"
+  type        = number
+  default     = 2
+}
 
 variable "min_size" {
-    description = "Minimum size of the Auto Scaling group"
-    type        = number
-    default     = 1
+  description = "Minimum number of EC2 instances"
+  type        = number
+  default     = 1
 }
 
 variable "max_size" {
-    description = "Maximum size of the Auto Scaling group"
-    type        = number
-    default     = 3
+  description = "Maximum number of EC2 instances"
+  type        = number
+  default     = 4
 }
-
-variable "desired_capacity" {
-    description = "Desired capacity of the Auto Scaling group"
-    type        = number
-    default     = 2
-}
-
-
-
-
-
