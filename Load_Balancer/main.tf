aa84f5f67d3c257e89421a0f91021ea0b1118a55
Abhishek Wathore <abhishekbwathore@gmail.com>
2025-06-01 19:42:24 +0530

    Load Balancing Module

--- Load_Balancer/main.tf
+++ Load_Balancer/main.tf
@@ -1,3 +1,4 @@
+# Application Load Balancer
 resource "aws_lb" "main" {
   name               = var.aws_lb_name
   internal           = var.aws_lb_internal
@@ -10,21 +11,20 @@ resource "aws_lb" "main" {
   }
 }
 
+# Target Group for the ALB
 resource "aws_lb_target_group" "tg" {
-  name     = var.aws_lb_target_group_name
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = var.vpc_id
+  name        = var.aws_lb_target_group_name
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = var.vpc_id
   target_type = "instance"
-}
 
-resource "aws_lb_listener" "listener" {
-  load_balancer_arn = aws_lb.main.arn
-  port              = 80
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.tg.arn
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200"
   }
 }
