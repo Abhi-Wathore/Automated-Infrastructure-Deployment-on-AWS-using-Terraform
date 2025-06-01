1843367e9ecc061c02a4214e2eb7c6eb16df039c
Abhishek Wathore <abhishekbwathore@gmail.com>
2025-06-01 17:26:11 +0530

    VPC Module

--- VPC/output.tf
+++ VPC/output.tf
@@ -9,12 +9,12 @@ value = aws_vpc.mainvpc.id
 
 output "public_subnet_ids" {
 description = "List of public subnet IDs"
-value = aws_subnet.pub-subnet.id
+value = aws_subnet.pub_subnet[*].id
 }
 
 output "private_subnet_ids" {
 description = "List of private subnet IDs"
-value = aws_subnet.pvt-subnet.id
+value = aws_subnet.pvt_subnet[*].id
 }
 
 output "vpc_cidr_block" {
@@ -37,6 +37,9 @@ description = "ID of the default security group"
 value = data.aws_security_group.mysg.id
 }
 
-output "public_subnet_id" {
-  value = aws_subnet.public.id  # Or a list, like aws_subnet.public[*].id
-}
+# If you want to output a specific public subnet ID, ensure the resource exists and is named correctly.
+# Otherwise, you can remove this output if not needed, or output all public subnet IDs as above.
+# Example for a specific subnet:
+output "pub_subnet_id" {
+   value = aws_subnet.pub_subnet[0].id
+ }
